55c757b8a534961a89c1c0b4baa2151235cf8deb
Osiris <potterosiris@gmail.com>
2020-08-05 14:10:17 -0400

    inquirer init prompt

--- index.js
+++ index.js
@@ -4,4 +4,70 @@ require("console.table");
 
 init();
 
-const init = () => {};
+const init = () => {
+  inquirer.prompt([
+    {
+      type: "list",
+      name: "choice",
+      message: "manage system",
+      choices: [
+        {
+          name: "view employees",
+          value: "viewEmployees",
+        },
+        {
+          name: "view employees by department",
+          value: "viewEmployeesByDepartment",
+        },
+        {
+          name: "view employees by manager",
+          value: "viewEmployeesByManager",
+        },
+        {
+          name: "add employee",
+          value: "addEmployee",
+        },
+        {
+          name: "remove employee",
+          value: "removeEmployee",
+        },
+        {
+          name: "update employee role",
+          value: "updateEmployeeRole",
+        },
+        {
+          name: "update employee manager",
+          value: "updateEmployeeManager",
+        },
+        {
+          name: "view roles",
+          value: "viewRoles",
+        },
+        {
+          name: "add role",
+          value: "addRole",
+        },
+        {
+          name: "remove role",
+          value: "removeRole",
+        },
+        {
+          name: "view departments",
+          value: "viewDepartments",
+        },
+        {
+          name: "add department",
+          value: "addDepartment",
+        },
+        {
+          name: "remove department",
+          value: "removeDepartment",
+        },
+        {
+          name: "exit",
+          value: "exit",
+        },
+      ],
+    },
+  ]);
+};
